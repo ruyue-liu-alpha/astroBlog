6f67404e34d990f2efac544772b0a7f2a705b664
ruyueliu <ruyueliu16@gmail.com>
2023-10-11 15:57:24 +0800

    github  yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+
+export default defineConfig({
+  site: 'https://ruyue-liu-alpha/astroBlog.github.io',
+  base: '/my-repo',
+})
+
